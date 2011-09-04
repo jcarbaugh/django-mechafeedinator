61f3c737304729fa6e399e03156c0a3a35a3efa9
Jeremy Carbaugh <jcarbaugh@gmail.com>
2011-09-04 10:45:16 -0400

    initial commit

---
+++ mechafeedinator/core.py
@@ -0,0 +1,76 @@
+from mechafeedinator.models import Feed, Item, Tag, Subscription
+import datetime
+import feedparser
+
+def update_feed(feed):
+
+    now = datetime.datetime.utcnow()
+
+    rss = feedparser.parse(feed.url)
+
+    for entry in rss.entries:
+
+        if not 'id' in entry:
+            # log error and skip to next entry
+            continue
+
+        try:
+
+            # update existing item, if it exists
+
+            item = Item.objects.get(feed=feed, uid=entry.id)
+            update_item(item, entry)
+
+        except Item.DoesNotExist:
+
+            # otherwise create a new item and populate from entry
+
+            item = Item(
+                feed=feed,
+                uid=entry.id,
+                title=entry.title,
+                link=entry.link,
+                date_fetched=now,
+            )
+            update_item(item, entry)
+
+    feed.last_fetched = now
+    feed.save()
+
+def update_item(item, entry):
+
+    item.summary = entry.get('summary', '')
+    item.save()
+
+    # get item content
+
+    item.content.delete()
+
+    if 'content' in entry:
+        for content in entry.content:
+            item.content.create(
+                value=content.get('value', ''),
+                mimetype=content.get('type', '')
+            )
+
+    # load tags from entry
+
+    item.tags.delete()
+
+    if 'tags' in entry:
+        for name in entry.tags:
+            item.tags.create(
+                name=name.term,
+                item=item,
+            )
+
+    # load enclosures
+
+    item.enclosures.delete()
+
+    if 'enclosures' in entry:
+        for enclosure in entry.enclosures:
+            item.enclosures.create(
+                url=enclosure.href,
+                mimetype=enclosure.get('type', ''),
+            )
---
+++ mechafeedinator/models.py
@@ -0,0 +1,75 @@
+from django.contrib.auth.models import User
+from django.db import models
+import datetime
+
+class Feed(models.Model):
+    url = models.URLField(verify_exists=False)
+    title = models.CharField(max_length=255, blank=True)
+    link = models.URLField(verify_exists=False, blank=True)
+    description = models.TextField(blank=True)
+    ttl = models.IntegerField(default=60)
+    date_added = models.DateTimeField(auto_now_add=True)
+    last_fetched = models.DateTimeField(blank=True, null=True)
+    next_fetch = models.DateTimeField()
+
+    class Meta:
+        ordering = ('title',)
+
+    def __unicode__(self):
+        return self.title
+
+    def save(self, **kwargs):
+        # do some date manipulating
+        super(Feed, self).save(**kwargs)
+
+class Item(models.Model):
+    feed = models.ForeignKey(Feed, related_name="items")
+    uid = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    link = models.URLField(verify_exists=False, blank=True)
+    summary = models.TextField(blank=True)
+    author_name = models.CharField(max_length=255, blank=True)
+    author_email = models.EmailField(blank=True, null=True)
+    author_uri = models.URLField(verify_exists=False, blank=True, null=True)
+    date_published = models.DateTimeField(blank=True, null=True)
+    date_updated = models.DateTimeField(blank=True, null=True)
+    date_fetched = models.DateTimeField()
+
+    class Meta:
+        ordering = ('-date_published',)
+
+    def __unicode__(self):
+        return u"%s: %s" % (self.feed.title, self.title)
+
+class Content(models.Model):
+    item = models.ForeignKey(Item, related_name="content")
+    value = models.CharField(blank=True)
+    mimetype = models.CharField(max_length=128, blank=True)
+
+    def __unicode__(self):
+        return u"[%s] %s" % (self.mimetype, self.value[:30])
+
+class Enclosure(models.Model):
+    item = models.ForeignKey(Item, related_name="enclosures")
+    url = models.URLField(verify_exists=False)
+    mimetype = models.CharField(max_length=128, blank=True)
+
+    def __unicode__(self):
+        return self.url
+
+class Tag(models.Model):
+    item = models.ForeignKey(Item, related_name="tags")
+    value = models.CharField(max_length=128)
+
+    class Meta:
+        ordering = ('value',)
+
+    def __unicode__(self):
+        return self.value
+
+class Subscription(models.Model):
+    user = models.ForeignKey(User, related_name='subscriptions')
+    feeds = models.ManyToManyField(Feed, related_name='subscriptions')
+
+    class Meta:
+        pass
